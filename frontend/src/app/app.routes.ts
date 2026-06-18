9a085081a8c2088edc0a216571863cd0aee190f3
Geovana Gonçalo <mariageovana82268@gmail.com>
2026-06-18 12:03:11 -0300

    feat: criar páginas e rotas iniciais do frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
+import { NotFound } from './pages/not-found/not-found';
+import { Register } from './pages/register/register';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
